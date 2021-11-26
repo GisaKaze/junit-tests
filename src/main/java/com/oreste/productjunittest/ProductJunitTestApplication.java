876dc44cae0137b89e8dd3c435c1a31b7e892770
oreste-abizera <oresteabizera11@gmail.com>
2021-11-26 08:25:55 +0200

    Initial Commit

---
+++ src/main/java/com/oreste/productjunittest/ProductJunitTestApplication.java
@@ -0,0 +1,13 @@
+package com.oreste.productjunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductJunitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductJunitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oreste/productjunittest/ProductJunitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreste.productjunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductJunitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
