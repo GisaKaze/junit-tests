945c8c56d058eb509063c346d3e0b837aadd1fd1
oreste-abizera <oresteabizera11@gmail.com>
2021-11-26 10:28:46 +0200

    default data for testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into product(id, name, price, quantity) values (1, 'Shoes',100, 10);
+insert into product(id, name, price, quantity) values (2, 'Watch',300, 20);
+insert into product(id, name, price, quantity) values (3, 'Earphones',300, 20);
+insert into product(id, name, price, quantity) values (4, 'Mouse',300, 20);
